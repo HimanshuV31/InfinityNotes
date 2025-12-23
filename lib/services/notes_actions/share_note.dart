5939af32e29f7f4df4dbdc8d2aa0cb8013a07a9c
Himanshu Vaishnav <himanshudv31@gmail.com>
2025-12-23 20:32:28 +0530

    231225, Deep Linking + GitHub Pages setup

--- lib/services/notes_actions/share_note.dart
+++ lib/services/notes_actions/share_note.dart
@@ -1,14 +1,23 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:infinity_notes/services/cloud/cloud_note.dart';
 
-Future<void> shareNote ({required CloudNote note, required BuildContext context})async {
-    final content = 'Title: ${note.title}\n\n${note.text}';
-    SharePlus.instance.share(
-      ShareParams(
-        text: content,
-        title: 'Share Note',
-        subject: note.title.isNotEmpty ? note.title : "Untitled Note",
-      ),
-    );
+Future<void> shareNote({
+  required CloudNote note,
+  required BuildContext context,
+}) async {
+  final noteId = note.documentId;
+  final deepLink = 'https://himanshuv31.github.io/InfinintyNotes/note/$noteId';
+
+  final shareText = note.title.isNotEmpty
+      ? '${note.title}\n\n$deepLink\n\nShared via Infinity Notes'
+      : 'Check out my note: $deepLink\n\nShared via Infinity Notes';
+
+  await SharePlus.instance.share(
+    ShareParams(
+      text: shareText,
+      title: 'Share Note',
+      subject: note.title.isNotEmpty ? note.title : 'Shared Note from Infinity Notes',
+    ),
+  );
 }
